61e5e26da2c8af2b392a09a26c9575ff19e98c5e
swilliams <solomon.fwilliams@gmail.com>
2024-05-26 23:05:09 -0500

    first commit

--- Jira/install-jira.sh
+++ Jira/install-jira.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl create ns jira
-kubectl -n jira create secret generic jira-secret --from-literal=username='jira' --from-literal=password='jira123'
+kubectl -n jira create secret generic mssql-secret --from-literal=username='jira' --from-literal=password='jira123'
 helm upgrade -i jira atlassian-data-center/jira --namespace jira --create-namespace  --values jira-values.yaml 
 kubectl get pod -n jira
 kubectl get svc -n jira
